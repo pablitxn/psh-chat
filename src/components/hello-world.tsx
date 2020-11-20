7488a147fe7d8890ce116b0296e29fc7df4e8bd1
Pablo Coronel <me@pablitxn.io>
2020-11-20 19:40:11 -0300

    feat(app): se crearon los directorios del proyecto

---
+++ src/components/hello-world.tsx
@@ -0,0 +1,7 @@
+// import styles from './hello-world.module.scss'
+
+export default function HelloWorld() {
+	return (
+		<div className="">Hello World, I am being styled using SCSS Modules!</div>
+	);
+}
